5c4db97d9febf594267a288f527a01ad119eeee0
Song Zhaohui <zhaohui.song@studio.unibo.it>
2021-11-08 18:50:12 +0100

    First functional version of  getPath

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -1,6 +1,5 @@
 package it.unibo.oop.lab06.generics1;
 
-
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -12,9 +11,8 @@ import java.util.Set;
 
 public class GraphImpl<N> implements Graph<N> {
 
-	
-	private Map <N, Set<N>> nodes;
-	
+	private Map<N, Set<N>> nodes;
+
 	public GraphImpl() {
 		super();
 		this.nodes = new LinkedHashMap<>();
@@ -27,19 +25,19 @@ public class GraphImpl<N> implements Graph<N> {
 
 	@Override
 	public void addNode(N node) {
-		if(!nodeAlreadyExist(node)) {
+		if (!nodeAlreadyExist(node)) {
 			nodes.put(node, new LinkedHashSet<N>());
 		}
 	}
-	
-	private boolean nodeAlreadyExist(N node){
+
+	private boolean nodeAlreadyExist(N node) {
 		return nodes.containsKey(node);
 	}
 
 	@Override
 	public void addEdge(N source, N target) {
 		Set<N> edgeds = nodes.get(source);
-		if(edgeds != null) {
+		if (edgeds != null) {
 			edgeds.add(target);
 		}
 	}
@@ -47,7 +45,7 @@ public class GraphImpl<N> implements Graph<N> {
 	@Override
 	public Set<N> nodeSet() {
 		Set<N> allnodes = new LinkedHashSet<N>();
-		for(var entry : nodes.entrySet()) {
+		for (var entry : nodes.entrySet()) {
 			allnodes.add(entry.getKey());
 		}
 		return allnodes;
@@ -56,7 +54,7 @@ public class GraphImpl<N> implements Graph<N> {
 	@Override
 	public Set<N> linkedNodes(N node) {
 		Set<N> edgeds = nodes.get(node);
-		if(edgeds != null) {
+		if (edgeds != null) {
 			return new LinkedHashSet<>(edgeds);
 		}
 		return new LinkedHashSet<N>(Collections.emptySet());
@@ -66,28 +64,36 @@ public class GraphImpl<N> implements Graph<N> {
 	public List<N> getPath(N source, N target) {
 		Set<N> alreadyVisited = new LinkedHashSet<>();
 		Queue<N> nodeToVisit = new LinkedList<>();
-		nodeToVisit.add(source);
 		Map<N, N> itsParent = new LinkedHashMap<>();
-		
-			while(!nodeToVisit.isEmpty()){
-				N node = nodeToVisit.poll();
-				if(node.equals(target)) {
-					System.out.println(node);
-					break;
+		List<N> path = new LinkedList<>();
+		nodeToVisit.add(source);
+
+		while (!nodeToVisit.isEmpty()) {
+			N node = nodeToVisit.poll();
+			if (node.equals(target)) {
+				break;
+			}
+
+			alreadyVisited.add(node);
+			for (var edgeds : this.linkedNodes(node)) {
+				if (!alreadyVisited.contains(edgeds)) {
+					nodeToVisit.add(edgeds);
+					itsParent.put(edgeds, node);
 				}
-				
-					alreadyVisited.add(node);
-					for(var edgeds : this.linkedNodes(node)) {
-						if(!alreadyVisited.contains(edgeds)) {
-						nodeToVisit.add(edgeds);
-						itsParent.put(edgeds, node);
-						
-					}
-			}			
-			
+			}
 		}
-		
-		return null;
+		nodeToVisit = new LinkedList<>();
+		nodeToVisit.add(target);
+		while(!nodeToVisit.isEmpty()) {
+			N node = nodeToVisit.poll();
+			path.add(node);
+			if(node.equals(source)) {
+				break;
+			}
+			nodeToVisit.add(itsParent.get(node));
+		}
+		Collections.reverse(path);
+		return path;
 	}
-	
+
 }
